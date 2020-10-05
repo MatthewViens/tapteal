de8d7bb40636ee3a008833a9d44b434b9810f14a
Matthew Viens <matthew.j.viens@gmail.com>
2020-10-04 17:22:40 -0700

    add event listener for header on scroll

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const header = document.querySelector('header');
 const hamburger = document.querySelector('.menu-btn__burger');
 const hamburgerTopColor = window.getComputedStyle(hamburger, ':before').getPropertyValue('background')
 const hamburgerBottomColor = window.getComputedStyle(hamburger, ':after').getPropertyValue('background')
+const logo = document.querySelector('.logo-img')
 
 let menuOpen = false;
 menuBtn.addEventListener('click', (e) => {
@@ -15,13 +16,15 @@ menuBtn.addEventListener('click', (e) => {
     menuOpen = false;
     menuBtn.classList.remove('open');
     nav.style.left = '100%';
+    if (window.scrollY > 10) {
+      hamburger.style.backgroundColor = "#272727";
+    }
     // header.style.backgroundColor = 'transparent';
     // header.style.color = '#fff';
   } else {
     menuOpen = true;
     menuBtn.classList.add('open');
-    // header.style.backgroundColor = '#fff';
-    // header.style.color = '#000';
+    hamburger.style.backgroundColor = "#eaebe4";
     nav.style.left = '50%';
   }
   console.log('hamburger clicked')
@@ -36,6 +39,20 @@ container.addEventListener('click', (e) => {
   }
   console.log('container clicked')
   console.log(menuOpen)
+})
+
+window.addEventListener('scroll', () => {
+  height = window.scrollY
+  if (height > 10) {
+    hamburger.style.backgroundColor = "#272727";
+    logo.src = "./img/logo-black.svg";
+    // hamburgerTopColor = "#272727"
+    header.classList.add('background-scrolled')
+  } else {
+    hamburger.style.backgroundColor = "#eaebe4";
+    header.classList.remove('background-scrolled')
+    logo.src = "./img/logo.svg"
+  }
 })
 
 let viewportHeight = container.offsetHeight
